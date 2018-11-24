c16df5699369edf23cb138f3a476b6f27b4554e1
Alex Barney <thealexbarney@gmail.com>
2018-11-24 16:11:50 -0600

    Add project files.

---
+++ SmashNxTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmashNxTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
